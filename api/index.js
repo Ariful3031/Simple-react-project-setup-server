6eb043e9ea37e89f182d552aad7939cf27aab6f8
Ariful3031 <hero.ariful31@gmail.com>
2026-04-05 15:56:22 +0600

    update at local system

--- api/index.js
+++ api/index.js
@@ -770,8 +770,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -784,9 +784,9 @@ app.get('/', (req, res) => {
     res.send('Simple react projects setup is running!')
 })
 
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 module.exports = app;
 module.exports.handler = serverless(app);
