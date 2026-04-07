d568cdbd3ba4a6c2f400bdaaf19913eac52aa576
ARIF <hero.ariful31@gamil.comm>
2026-04-07 23:48:16 +0600

    update all deploy tast

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db("Simple-Project");
         const coursesCollection = db.collection("courses");
@@ -770,8 +770,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -784,9 +784,9 @@ app.get('/', (req, res) => {
     res.send('Simple react projects setup is running!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
 
 module.exports = app;
 module.exports.handler = serverless(app);
